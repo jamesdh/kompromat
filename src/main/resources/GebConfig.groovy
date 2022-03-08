fdc5fe9bc72a140451efb0b2893f9f7fd2f58ae3
James Hardwick <jamesdh@gmail.com>
2022-03-08 11:06:36 -0600

    Switch to chrome instead of firefox
    The chrome driver allows us to disable certain flags which help sites detect that it's running automated

--- src/main/resources/GebConfig.groovy
+++ src/main/resources/GebConfig.groovy
@@ -1,29 +1,34 @@
-import org.openqa.selenium.Point
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverLogLevel
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxDriverLogLevel
 import org.openqa.selenium.firefox.FirefoxOptions
 
 import java.util.concurrent.TimeUnit
 
+// driver = {
+//     FirefoxOptions options = new FirefoxOptions()
+//     options.logLevel = FirefoxDriverLogLevel.ERROR
+//     WebDriver driver = new FirefoxDriver(options)
+//     driver.executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+//     driver.manage().window().maximize()
+//     driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS)
+//     return driver
+// }
+
 driver = {
-    FirefoxOptions options = new FirefoxOptions()
-    options.logLevel = FirefoxDriverLogLevel.ERROR
-    WebDriver driver = new FirefoxDriver(options)
+    ChromeOptions options = new ChromeOptions()
+    options.setExperimentalOption("excludeSwitches", ["enable-automation"])
+    options.setExperimentalOption("useAutomationExtension", false)
+    options.addArguments("--disable-blink-features=AutomationControlled")
+    options.logLevel = ChromeDriverLogLevel.OFF
+    ChromeDriver driver = new ChromeDriver(options)
+    driver.executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     // Point point = new Point(3008, -690)
     // driver.manage().window().setPosition(point)
     driver.manage().window().maximize()
-    driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS)
+    driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS)
     return driver
 }
-
-// driver = {
-//     ChromeOptions options = new ChromeOptions()
-//     options.logLevel = ChromeDriverLogLevel.OFF
-//     ChromeDriver driver = new ChromeDriver(options)
-//     Point point = new Point(3008, -690)
-//     driver.manage().window().setPosition(point)
-//     driver.manage().window().maximize()
-//     driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS)
-//     return driver
-// }
